86d65c916b33a1328117a842d5c320616dab6809
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 00:59:18 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-montague[hero,status = "dead"]
+montague[:hero] 
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
